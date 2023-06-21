a0891cddd401ce899df6a356df99d70d8a76f904
tp <61617660+ittp@users.noreply.github.com>
2023-06-21 03:03:28 +0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import { createRoot } from "react-dom/client";
 
 import App from "./App";
+import Content from "./Drawer";
 import "antd/dist/antd.css";
-
+import interfaces from "./demo.json";
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
-root.render(<App />);
+root.render(
+  <>
+    <App />
+    <Content data={interfaces} />
+  </>
+);
